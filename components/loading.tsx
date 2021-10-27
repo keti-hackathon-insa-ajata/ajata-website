74058eb31f00ef7b8f1a49f2b21baef50e90fed1
Arnaud Vergnet <arnaud.vergnet@mailo.com>
2021-10-27 10:06:21 +0200

    feat: add loading spinner

--- components/loading.tsx
+++ components/loading.tsx
@@ -1,9 +1,10 @@
 import styles from '../styles/Loading.module.css';
+import { CircularProgress } from '@mui/material';
 
 export default function Loading() {
   return (
     <div className={styles.container}>
-      <p className={styles.content}>Loading...</p>
+      <CircularProgress className={styles.content} />
     </div>
   );
 }
