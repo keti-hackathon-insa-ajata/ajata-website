c6414c8aec6b3b99607b99f01ebf275320fc21d7
pikouri <hok@etud.insa-toulouse.fr>
2021-11-09 23:56:15 +0100

    fix: CORS error (when sending POST to OM2M) fixed

--- constants/links.ts
+++ constants/links.ts
@@ -1,5 +1,5 @@
 export default {
   liveReports: '/api/mock', // http://192.168.43.129:12345/dangerReports
-  localMarkers: '/api/markers',
-  om2mEndpoint: 'localhost',
+  localMarkers: '/api/mock', //'/api/markers',
+  om2mEndpoint: '/api/om2m/~/mn-cse/mn-name/AjataSensor/DATA',
 };
--- next.config.js
+++ next.config.js
@@ -1,3 +1,12 @@
 module.exports = {
   reactStrictMode: true,
-}
+  // Proxy to avoid CORS error with OM2M
+  async rewrites() {
+    return [
+      {
+        source: '/api/om2m/:path*',
+        destination: 'http://localhost:8282/:path*',
+      },
+    ];
+  },
+};
