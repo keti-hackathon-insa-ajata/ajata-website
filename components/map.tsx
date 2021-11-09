6fd742659073670b0c8a0a7a740c889457868c9b
Arnaud Vergnet <arnaud.vergnet@mailo.com>
2021-11-09 18:41:01 +0100

    fix: undefined icon

--- components/map.tsx
+++ components/map.tsx
@@ -13,6 +13,16 @@ import L from 'leaflet';
 L.Icon.Default.imagePath = 'leaflet_images/';
 
 // Blue icon for unsynced reports
+const redIcon = new L.Icon({
+  iconUrl: 'leaflet_images/marker-icon.png',
+  shadowUrl: 'leaflet_images/marker-shadow.png',
+  iconSize: [25, 41],
+  iconAnchor: [12, 41],
+  popupAnchor: [1, -34],
+  shadowSize: [41, 41],
+});
+
+// Blue icon for unsynced reports
 const blueIcon = new L.Icon({
   iconUrl: 'leaflet_images/marker-icon-blue.png',
   shadowUrl: 'leaflet_images/marker-shadow.png',
@@ -47,12 +57,11 @@ export default function Map(props: Props) {
       />
       {props.markers
         ? props.markers.map((m, index) => {
-            console.log(m);
             return (
               <Marker
                 key={'marker' + index}
                 position={[m.latitude, m.longitude]}
-                icon={m.sync ? undefined : blueIcon}
+                icon={m.sync ? redIcon : blueIcon}
               >
                 <Popup>
                   <p>
