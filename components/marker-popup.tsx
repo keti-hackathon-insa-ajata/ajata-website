47ab4cf378593f8fae6b57aaa04c42efe06b5687
pikouri <hok@etud.insa-toulouse.fr>
2021-11-11 15:24:21 +0100

    feat: Marker popup redesigned

--- components/marker-popup.tsx
+++ components/marker-popup.tsx
@@ -2,6 +2,12 @@ import { Popup, PopupProps } from 'react-leaflet';
 import { InformationNode, LocalInformationNode } from '../types/api';
 import { Button } from '@mui/material';
 import DeleteIcon from '@mui/icons-material/DeleteOutline';
+import DateIcon from '@mui/icons-material/AccessTimeOutlined';
+import VehicleSpeedIcon from '@mui/icons-material/DirectionsCarFilledOutlined';
+import BicycleSpeedIcon from '@mui/icons-material/DirectionsBikeOutlined';
+import DistanceIcon from '@mui/icons-material/SocialDistanceOutlined';
+import SyncedIcon from '@mui/icons-material/SyncOutlined';
+import NotSyncedIcon from '@mui/icons-material/SyncDisabledOutlined';
 import { useRef } from 'react';
 
 type Props = PopupProps &
@@ -22,9 +28,19 @@ export default function MarkerPopup(props: Props) {
   const getSyncMessage = () => {
     if (props.local) {
       if (props.item.sync) {
-        return <p>SYNC</p>;
+        return (
+          <p>
+            <SyncedIcon sx={{ mr: 1, fontSize: 'medium', color: 'green' }} />
+            <strong>Synced</strong>
+          </p>
+        );
       } else {
-        return <p>NOT SYNC</p>;
+        return (
+          <p>
+            <NotSyncedIcon sx={{ mr: 1, fontSize: 'medium', color: 'red' }} />
+            <strong>Not synced</strong>
+          </p>
+        );
       }
     } else {
       return null;
@@ -33,21 +49,34 @@ export default function MarkerPopup(props: Props) {
 
   const ref = useRef();
 
+  const date = new Intl.DateTimeFormat('fr-FR', {
+    year: 'numeric',
+    month: '2-digit',
+    day: '2-digit',
+    hour: '2-digit',
+    minute: '2-digit',
+    second: '2-digit',
+  }).format(new Date(item.timestamp * 1000));
+
   return (
     <Popup ref={ref}>
+      <p>
+        <DateIcon sx={{ mr: 1, fontSize: 'medium' }} />
+        <strong>Date:</strong> {date}
+      </p>
+      <p>
+        <DistanceIcon sx={{ mr: 1, fontSize: 'medium' }} />
+        <strong>Distance:</strong> {item.distance} cm
+      </p>
+      <p>
+        <VehicleSpeedIcon sx={{ mr: 1, fontSize: 'medium' }} />
+        <strong>Vehicle speed:</strong> {item.object_speed} km/h
+      </p>
+      <p>
+        <BicycleSpeedIcon sx={{ mr: 1, fontSize: 'medium' }} />
+        <strong>Bike speed:</strong> {item.bicycle_speed} km/h
+      </p>
       {getSyncMessage()}
-      <p>
-        <strong>Date:</strong> {item.timestamp}
-      </p>
-      <p>
-        <strong>Distance:</strong> {item.distance}
-      </p>
-      <p>
-        <strong>Object speed:</strong> {item.object_speed}
-      </p>
-      <p>
-        <strong>Bike speed:</strong> {item.bicycle_speed}
-      </p>
       {props.local && !props.item.sync ? (
         <Button
           size={'small'}
--- constants/links.ts
+++ constants/links.ts
@@ -1,5 +1,5 @@
 export default {
-  liveReports: '/api/mock', // http://192.168.43.129:12345/dangerReports
+  liveReports: 'http://192.168.43.171:12345/dangerReports?dangerous=true', // '/api/mock',
   localMarkers: '/api/markers',
   om2mEndpoint:
     '/api/om2m/~/' + process.env.om2mMnCseName + '/AjataSensor/DATA',
